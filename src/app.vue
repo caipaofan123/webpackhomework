f21f47a54a1022ded216eb1dda76f5e5545f3838
caipaofan <954698621@qq.com>
2022-06-27 19:28:45 +0800

    初步打包

---
+++ src/app.vue
@@ -0,0 +1,19 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: 'HomeworkdemoApp',
+
+  data() {
+    return {};
+  },
+
+  mounted() {},
+
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped></style>
