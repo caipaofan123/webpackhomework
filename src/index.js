dd89da4c11bec31a1b8c9381cc3d96931b166bff
caipaofan <954698621@qq.com>
2022-06-27 20:46:52 +0800

    完成了图片加载打包

--- src/index.js
+++ src/index.js
@@ -3,3 +3,10 @@ import './tabs.js'
 
 // import './styles/index.css'
 import './styles/index.less'
+
+
+import imgUrl from './assets/1.gif';
+
+let img = document.createElement('img');
+img.src = imgUrl;
+document.body.appendChild(img);
